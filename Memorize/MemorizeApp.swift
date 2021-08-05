cd56564d9fbbea25001f939e67f0bedf16d43288
csemozid <csemozid@gmail.com>
2021-08-05 06:04:56 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Md. Abdul Mozid on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
